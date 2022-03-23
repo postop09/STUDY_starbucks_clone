33179da4aa716606990e943b16b964774356a405
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-03-23 14:46:31 +0900

    두번째 무슨 기능

--- legendkim/app/src/main/java/test/map/testtest/MainActivity.kt
+++ legendkim/app/src/main/java/test/map/testtest/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //두번째 기능 오예
 
     }
 }
