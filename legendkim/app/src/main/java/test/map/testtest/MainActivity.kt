755ae1a2d89b9a8bf646742914608b4fb7f7645a
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-03-23 14:46:06 +0900

    무슨무슨기능

--- legendkim/app/src/main/java/test/map/testtest/MainActivity.kt
+++ legendkim/app/src/main/java/test/map/testtest/MainActivity.kt
@@ -3,9 +3,12 @@ package test.map.testtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//무슨무슨 기능
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
